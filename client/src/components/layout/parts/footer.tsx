5f59d3a7a20523ec9dd1e5b6e9189f24f2c990cc
Mahmoud Dahdouh <mahmooddahdooh99@gmail.com>
2025-02-07 10:56:09 +0200

    setup client

---
+++ client/src/components/layout/parts/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="h-1 rounded-full bg-gray-50 w-full"></div>
+      </div>
+      <div className="container py-4">
+        <h2 className="font-semibold text-center">
+          CTX Technical Test - Made with ❤️
+        </h2>
+      </div>
+    </footer>
+  )
+}
---
+++ client/src/components/layout/parts/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container py-4">
+        <h2 className="text-2xl font-semibold">Simple notes app</h2>
+      </div>
+      <div className="container">
+        <div className="h-1 rounded-full bg-gray-50 w-full"></div>
+      </div>
+    </header>
+  )
+}
